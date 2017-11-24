d41091386c13f05969a0bcde2dc2b53344b00b8a
Simon Lebedev <simonmoshenko@gmail.com>
2017-11-24 15:08:23 +0600

    Play around with bootstrap

--- latestInstance/settings.py
+++ latestInstance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0-!k1ci0ig7il_zmqcuf#f)^e(6jnd^glz=hw0bw2n&qf)95yx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []#['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'lebedev.ddns.net']
 
 
 # Application definition
